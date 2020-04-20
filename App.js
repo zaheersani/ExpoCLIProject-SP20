620f6bc39fc7ef7f2fc1bc9c9e6a547c73390ba3
zaheersani <sani.zaheer@gmail.com>
2020-04-20 15:40:34 +0500

    Event Handler added

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const App = () => {
+
+  const buttonClicked = (msg) => {
+    alert("Message: " + msg);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerView}>
+        <Text style={styles.text}>First Line</Text>
+      </View>
+      <View style={styles.innerView}>
+        <Text style={styles.text}>Second Line</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <View style={{ padding: 10 }}>
+          <Button title="Press Me" onPress={buttonClicked.bind(this, "Button 1 is Pressed!")}></Button>
+        </View>
+        <View style={{ padding: 10 }}>
+          <Button title="Press Me" onPress={buttonClicked.bind(this, "Button 1 is Pressed!")}></Button>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: 20
+    },
+    innerView: {
+      padding: 10
+    },
+    text: {
+      fontSize: 24,
+    },
+    buttonsContainer: {
+      flexDirection: "row"
+    }
+  }
+);
+
+export default App;
