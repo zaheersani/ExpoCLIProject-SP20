9dec16a1ecf489bb0a22ac5c97ff67d05c9aa214
zaheersani <sani.zaheer@gmail.com>
2020-04-21 15:30:20 +0500

    React Hooks - Use of JS Object instead of Primitive DataType

--- App.js
+++ App.js
@@ -2,33 +2,26 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const App = () => {
-  const [getFirstName, setFirstName] = useState('First Name Here!');
-  const [getLastName, setLastName] = useState('Last Name Here!');
+  const [getName, setName] = useState({ fName: 'FirstName', lName: 'LastName' });
 
-
-
-  const buttonClicked = (msg) => {
-    alert("Msg: " + msg);
-  }
-
-  const updateName = (name) => {
-    setFirstName(name);
+  const updateFirstName = (firstname) => {
+    setName({ fName: firstname, lName: getName.lName });
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.innerView}>
-        <Text style={styles.text}>{getFirstName}</Text>
+        <Text style={styles.text}>{getName.fName}</Text>
       </View>
       <View style={styles.innerView}>
-        <Text style={styles.text}>{getLastName}</Text>
+        <Text style={styles.text}>{getName.lName}</Text>
       </View>
       <View style={styles.buttonsContainer}>
         <View style={{ padding: 10 }}>
-          <Button title="Update FirstName" onPress={updateName.bind(this, "John")}></Button>
+          <Button title="Update FirstName" onPress={updateFirstName.bind(this, "John2")}></Button>
         </View>
         <View style={{ padding: 10 }}>
-          <Button title="Update LastName" onPress={() => setLastName("Bengston")}></Button>
+          <Button title="Update LastName" onPress={() => setName({ fName: getName.fName, lName: "Bengston2" })}></Button>
         </View>
       </View>
     </View>
