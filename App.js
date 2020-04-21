62a71c7c0034260d19e7a3fcbe40b872fb7ec7fc
zaheersani <sani.zaheer@gmail.com>
2020-04-21 15:20:23 +0500

    React Hooks, FirstName LastName Hook

--- App.js
+++ App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const App = () => {
+  const [getFirstName, setFirstName] = useState('First Name Here!');
+  const [getLastName, setLastName] = useState('Last Name Here!');
+
+
 
   const buttonClicked = (msg) => {
     alert("Msg: " + msg);
   }
 
+  const updateName = (name) => {
+    setFirstName(name);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.innerView}>
-        <Text style={styles.text}>First Line</Text>
+        <Text style={styles.text}>{getFirstName}</Text>
       </View>
       <View style={styles.innerView}>
-        <Text style={styles.text}>Second Line</Text>
+        <Text style={styles.text}>{getLastName}</Text>
       </View>
       <View style={styles.buttonsContainer}>
         <View style={{ padding: 10 }}>
-          <Button title="Press Me" onPress={buttonClicked.bind(this, "Button 1 is Pressed!")}></Button>
+          <Button title="Update FirstName" onPress={updateName.bind(this, "John")}></Button>
         </View>
         <View style={{ padding: 10 }}>
-          <Button title="Press Me" onPress={buttonClicked.bind(this, "Button 1 is Pressed!")}></Button>
+          <Button title="Update LastName" onPress={() => setLastName("Bengston")}></Button>
         </View>
       </View>
     </View>
